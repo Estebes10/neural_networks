19757cee8b5c096a2b7b799d2c81dc1e1da3d2a0
Juan Estebes González <a01204421@itesm.mx>
2018-11-12 19:04:32 -0600

    Change input parser

--- neural.py
+++ neural.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
+import fileinput
 
 def main():
     lines = []
-    inputs = 0
     #Parse lines of the input
-    inputs = fileinput.input()
-    training_size = fileinput.input()
-    tests_size = fileinput.input()
-    training_data = []
-    test_data = []
+    # inputs = int(fileinput.input())
+    # training_size = int(fileinput.input())
+    # tests_size = int(fileinput.input())
+    inputs = int(input())
+    training_size = int(input())
+    tests_size = int(input())
+    training_data_strings = []
+    test_data_strings = []
 
-    for line in range(0, training_size):
-        training_data.append(line)
+    for val in range(0, training_size):
+        # line = fileinput.input()
+        line = list(map(float, input().replace(" ", '').split(',')))
+        training_data_strings.append(line)
 
-    for line in range(0, tests_size):
-        test_data.append(line)
+    for val in range(0, tests_size):
+        # line = fileinput.input()
+        line2 = list(map(float, input().replace(" ", "").split(',')))
+        test_data_strings.append(line2)
 
     print(inputs)
     print(training_size)
     print(tests_size)
-    print(training_data)
-    print(test_data)
+    print(training_data_strings)
+    print(test_data_strings)
 
 if __name__ == "__main__":
     main()
